e32e73e8be994c21350106c66f5fb9faed799ab5
geoff <geoff.vargo@stonybrook.edu>
2017-12-20 01:51:22 -0500

    Fiddling around with the construction of layout.

--- nestabletextview/src/main/java/com/vargo/geoff/nestabletextview/NestableTextView.java
+++ nestabletextview/src/main/java/com/vargo/geoff/nestabletextview/NestableTextView.java
@@ -1,15 +1,26 @@
 package com.vargo.geoff.nestabletextview;
 
 import android.content.Context;
-import android.support.v7.widget.AppCompatTextView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 /**
  * Created by geoff on 12/19/2017.
  */
 
-public class NestableTextView extends AppCompatTextView {
+public class NestableTextView extends RelativeLayout {
+
+	protected TextView text = null;
+	protected NestableTextView child = null;
+
 	public NestableTextView(Context context) {
 		super(context);
+		text = new TextView(context);
+		child = new NestableTextView(context);
+
+		text.setText("asdf");
+
+		child.setTop(2);
+		child.setLeft(2);
 	}
 }
